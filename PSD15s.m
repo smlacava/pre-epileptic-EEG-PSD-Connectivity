69a508c3a565892d340bcfed721f92835c842dfe
smlacava <45989668+smlacava@users.noreply.github.com>
2019-01-20 15:41:39 +0100

    Update PSD15s.m

--- PSD15s.m
+++ PSD15s.m
@@ -17,17 +17,16 @@ for count=1:3
     outDir=strcat('/Users/simon/Desktop/',type(1),'30PSD15s/');
     cases=dir(fullfile(inDir,fil));
     
-    for i=1:length(cases)  %for each subject
+    for i=1:length(cases)
     
         EEG=load(strcat(inDir,cases(i).name));
-        %psd = zeros(size(EEG.sup,3),size(EEG.sup,1),n_PSD);
         psd=zeros(n_epochs, size(EEG.sup,1),n_bands);
-        for k = 1:n_epochs %for each epoch
+        for k = 1:n_epochs
         
-            for j=1:size(EEG.sup,1) %for each channel
+            for j=1:size(EEG.sup,1)
                 data=squeeze(EEG.sup(j,fs*k+1:fs*k+dt));
                 %data=squeeze(EEG.sup(j,:,k));             
-                [pxx,w]=pwelch(data,[],0,[],fs);  %6 periodi da 5 secondi           
+                [pxx,w]=pwelch(data,[],0,[],fs);          
                 deltaPower=sum(pxx(2:5));         %1-4 hz      
                 thetaPower=sum(pxx(5:9));         %4-8 hz          
                 alphaPower=sum(pxx(9:14));        %8-13 hz            
